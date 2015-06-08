40a695a9e1bbbda622b0a22e958986a5e9ddb8fd
Anatolii Petrovskyi <apetrovskyi@readdle.com>
2015-06-08 10:51:54 +0000

    decrypt object and has function

--- rdconfig.js
+++ rdconfig.js
@@ -12,7 +12,9 @@ var RDConfig = function(){
     if (fs.existsSync(envFilePath)) {
         var envFileContent = fs.readFileSync(envFilePath).toString().split(/\r?\n/);
         process.env['NODE_ENV'] = envFileContent[0];
-        this.cryptKey = envFileContent[1];
+        if(envFileContent.length > 1) {
+            this.cryptKey = envFileContent[1];
+        }
     }
 
     this.config = require('config');
@@ -22,26 +24,38 @@ RDConfig.prototype.encrypt = function(value) {
     var cipher = crypto.createCipher(this.cryptMethod, this.cryptKey);
     var crypted = cipher.update(value,'utf8', 'hex');
     crypted += cipher.final('hex');
-    return crypted;
+    return this.cryptedFlag + crypted;
 };
 
-RDConfig.prototype.decrypt = function(value){
-    var decipher = crypto.createDecipher(this.cryptMethod, this.cryptKey);
-    var dec = decipher.update(value, 'hex', 'utf8');
-    dec += decipher.final('utf8');
-    return dec;
-};
-
-RDConfig.prototype.get = function(property, undefined){
-    var configValue = this.config.get(property);
-
-    if (typeof configValue === 'string' && configValue.substr(0, this.cryptedFlag.length) === this.cryptedFlag) {
-        configValue = configValue.substr(this.cryptedFlag.length);
-
-        return this.decrypt(configValue);
+RDConfig.prototype.decrypt = function(obj){
+    if(typeof obj === 'string' && obj.substr(0, this.cryptedFlag.length) === this.cryptedFlag){
+        var secureValue = obj.substr(this.cryptedFlag.length);
+        var decipher = crypto.createDecipher(this.cryptMethod, this.cryptKey);
+        var dec = decipher.update(secureValue, 'hex', 'utf8');
+        dec += decipher.final('utf8');
+        return dec;
+    }else if(typeof obj === 'string'){
+        return obj;
     }
 
-    return configValue;
+    var decryptedObject = {};
+
+    for(var key in obj){
+        var value = obj[key];
+        decryptedObject[key] = this.decrypt(value);
+    }
+
+    return decryptedObject;
+};
+
+RDConfig.prototype.get = function(property){
+    var configValue = this.config.get(property);
+
+    return this.decrypt(configValue);
+};
+
+RDConfig.prototype.has = function(property){
+    return this.config.has(property);
 };
 
 RDConfig.prototype.getConfigObj = function(){
